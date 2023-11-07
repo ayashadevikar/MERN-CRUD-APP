397031b9b375d49e37a46cd703aac94307c4f8c0
ayashadevikar <ayashadevikar8@gmail.com>
2023-11-07 18:41:03 +0530

    font size added

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -33,7 +33,7 @@ const Users = () => {
     return (
         <> 
             <div className=" flex flex-col justify-center items-center">
-                <Link to="/create" className="bg-green-600 p-4">Add +</Link>
+                <Link to="/create" className="bg-green-600 p-4 border-2">Add +</Link>
                 <table className="w-[30%] text-center">
                     <thead >
                         <tr >
